0bbaa4ae6f8c46d7f8ed6cc9d288f4f9c272879a
Solhee Tucker <solheetucker@gmail.com>
2025-11-23 17:51:31 -0800

    Use Environment var for api_key, code clean up

--- apis/gemini.py
+++ apis/gemini.py
@@ -1,28 +1,31 @@
 # pip install requests google-genai
+import os
+
 import get_ensembl, requests
 from google import genai
 
-client = genai.Client(api_key = "")
+api_key = os.environ['GEMINI_API_KEY']
+client = genai.Client(api_key=api_key)
 
-rs = "rs6311" # user input
+rs = "rs6311"  # user input
 rs_num = rs[2:] if rs.lower().startswith("rs") else rs
 # request json data and parsing into a Python dictionary
 data = requests.get(f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rs_num}", timeout=30).json()
 
 system_rules = (
-  "Use ONLY the provided JSON text below. "
-  "If a fact isn't present, reply 'Not in JSON'. "
-  "Do NOT return compact JSON. Return an easy to read summary for novices"
+    "Use ONLY the provided JSON text below. "
+    "If a fact isn't present, reply 'Not in JSON'. "
+    "Do NOT return compact JSON. Return an easy to read summary for novices"
 
 )
 
 prompt = (
-  "Summarize this SNP from the JSON text: rs, gene name, chromosome, GRCh38 position, "
-  "alleles, clinical significance, and Diseases."
-  "Format it in an pretty way for a novice to read"
+    "Summarize this SNP from the JSON text: rs, gene name, chromosome, GRCh38 position, "
+    "alleles, clinical significance, and Diseases."
+    "Format it in an pretty way for a novice to read"
 )
 
-json_str = json.dumps(data, ensure_ascii=False) # converting to json
+json_str = json.dumps(data, ensure_ascii=False)  # converting to json
 
 resp = client.models.generate_content(
     model="gemini-2.0-flash-001",
@@ -38,5 +41,3 @@ resp = client.models.generate_content(
 
 print(resp.text)
 # to do: turn into a function so I can call it?
-
-
