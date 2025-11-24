764b70e95e0cdeb19e3c49c829c0ddcb14a8d40d
Heather Ho <heatherho@gmail.com>
2025-11-23 23:32:17 -0800

    update get_ensembl.py

--- codes/ncbi_api.py
+++ codes/ncbi_api.py
@@ -2,11 +2,10 @@ import requests
 
 
 def get_info_from_ncbi(rs):
-    # rs_num = rs[2:] if rs.lower().startswith("rs") else rs
     # request json data
     return requests.get(f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rs}",
                         timeout=30).json()  # return apis dictionary
-    # return json.dumps(python_dic, ensure_ascii=True)
+
 
 def get_citations(citations):
     return requests.get(f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rs}",
