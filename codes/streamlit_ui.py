ccc11131c3e265fad882d6d9cf2afa8d8fb75fab
Heather Ho <heatherho@gmail.com>
2025-11-23 19:08:56 -0800

    update streamlit_ui.py
    # Conflicts:
    #	codes/streamlit_ui.py

--- codes/streamlit_ui.py
+++ codes/streamlit_ui.py
@@ -8,11 +8,26 @@ from gemini_function import get_summary
 st.title("SNP summary")
 
 # create rs id input
+
 rs_input = st.text_input("SNP rs id", "Enter rs number")
 
+summary = None # initialize summary to 'None'
+
 # display summary
-summary = get_summary(rs_input)
+if rs_input.isalpha():
+    st.error("Put in numbers only")
+try:
+    summary = get_summary(rs_input)
+except NameError:
+    st.error("Invalid input")
+except requests.exceptions.JSONDecodeError:
+    st.error("Requests JSONDecodeError")
+except Exception as e:
+    st.error(f"An exception occurred: {e}")
 
 # create submit button
-if st.button("Submit", type="primary"):
-    st.markdown(summary, unsafe_allow_html=True)  # create markdown for summary
+if st.button("Enter", type="primary"):
+    if summary:
+        st.markdown(summary, unsafe_allow_html=True)  # create markdown for summary
+    else:
+        st.error("Invalid input: Does your id exist?")
