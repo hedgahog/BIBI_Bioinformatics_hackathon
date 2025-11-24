1f38ea2843cf8ee66abf26234cd83a65ab77559c
Solhee Tucker <solheetucker@gmail.com>
2025-11-23 19:14:52 -0800

    use simple input handling

--- codes/streamlit_ui.py
+++ codes/streamlit_ui.py
@@ -11,23 +11,17 @@ st.title("SNP summary")
 
 rs_input = st.text_input("SNP rs id", "Enter rs number")
 
-summary = None # initialize summary to 'None'
+output = None  # initialize summary to 'None'
 
 # display summary
 if rs_input.isalpha():
-    st.error("Put in numbers only")
-try:
-    summary = get_summary(rs_input)
-except NameError:
-    st.error("Invalid input")
-except requests.exceptions.JSONDecodeError:
-    st.error("Requests JSONDecodeError")
-except Exception as e:
-    st.error(f"An exception occurred: {e}")
+    output = "Put in numbers only"
+else:
+    output = get_summary(rs_input)
 
 # create submit button
 if st.button("Enter", type="primary"):
-    if summary:
-        st.markdown(summary, unsafe_allow_html=True)  # create markdown for summary
+    if output:
+        st.markdown(output, unsafe_allow_html=True)  # create markdown for summary
     else:
         st.error("Invalid input: Does your id exist?")
