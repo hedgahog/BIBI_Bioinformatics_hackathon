043139e7c8befbe5be7a14c1f8336a55cf4b2324
Solhee Tucker <solheetucker@gmail.com>
2025-11-23 18:57:01 -0800

    move ensembl api call

--- codes/get_ensembl.py
+++ codes/get_ensembl.py
@@ -9,6 +9,9 @@ def get_html(rs):
     # rs_num = rs[2:] if rs.lower().startswith("rs") else rs
     # request json data
     r = requests.get(f"https://useast.ensembl.org/Homo_sapiens/Variation/Phenotype?v=rs{rs}", timeout=30)
+    params = {"phenotypes": 1}  # include disease/phenotype data
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    # r = requests.get(url, params=params, headers=headers, timeout=30)
     r.raise_for_status()
     return r.text
 
