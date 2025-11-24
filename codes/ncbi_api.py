214f94ae131e86f365fad92bd0b0ae6eb5f7049d
Solhee Tucker <solheetucker@gmail.com>
2025-11-23 23:15:01 -0800

    Get citations func

--- codes/ncbi_api.py
+++ codes/ncbi_api.py
@@ -7,3 +7,11 @@ def get_info_from_ncbi(rs):
     return requests.get(f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rs}",
                         timeout=30).json()  # return apis dictionary
     # return json.dumps(python_dic, ensure_ascii=True)
+
+def get_citations(citations):
+    return requests.get(f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{rs}",
+                        timeout=30).json()['citations']
+
+if __name__ == "__main__":
+    result = get_info_from_ncbi("6311")
+    print(result['citations'])
